e35b39b17369032b875635c9a99225514b9a3b3f
NaimKabir <kabir.naim@gmail.com>
2018-02-04 15:20:28 -0800

    More expmts added to main

--- experiment.py
+++ experiment.py
@@ -21,14 +21,16 @@ class Experiment(object):
 # Class for performing a stratified shuffle for a single experimental run
 class StratifiedExperiment(Experiment):
 
-    def __init__(self, model, criterion, features, labels):
+    def __init__(self, model, criterion, features, labels, test_size=0.2):
+
+        self.test_size = test_size
 
         super(StratifiedExperiment, self).__init__(model, criterion, features, labels)
 
     # In this partition I'm doing a stratified shuffle
     def partition(self, features, labels):
 
-        shuf = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
+        shuf = StratifiedShuffleSplit(n_splits=1, test_size=self.test_size)
         splits = shuf.split(features, labels)
 
         for train_idx, test_idx in splits:
@@ -38,13 +40,15 @@ class StratifiedExperiment(Experiment):
 
         train_idx, test_idx = self.partition(self.features, self.labels)
 
+        print("Train Size : %d  Test Size : %d" % (train_idx.shape[0], test_idx.shape[0]))
+
         train_features, train_labels = self.features[train_idx, :], self.labels[train_idx]
         test_features, test_labels = self.features[test_idx, :], self.labels[test_idx]
 
         self.model.fit(train_features, train_labels)
 
-        train_metric = self.criterion(self.model.predict(train_features), train_labels)
-        test_metric = self.criterion(self.model.predict(test_features), test_labels)
+        train_metric = self.criterion(train_labels, self.model.predict(train_features))
+        test_metric = self.criterion(test_labels, self.model.predict(test_features))
 
         return train_metric, test_metric
 
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from dataloader import WayneLoanApprovalLoader
 from experiment import StratifiedExperiment
 import numpy as np
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.ensemble.forest import RandomForestClassifier
 from sklearn.ensemble.bagging import BaggingClassifier
+from sklearn.ensemble.weight_boosting import AdaBoostClassifier
+
+from matplotlib import pyplot as plt
 
 wayne_all = WayneLoanApprovalLoader(savename='wayne_all', csvfile='wayne_county_2007_2014.tsv')
 
@@ -12,14 +16,18 @@ wayne_all = WayneLoanApprovalLoader(savename='wayne_all', csvfile='wayne_county_
 lr1 = LogisticRegression()
 lr2 = LogisticRegression()
 
+ada1 = AdaBoostClassifier()
+ada2 = AdaBoostClassifier()
+
+timemodels = [lr1, lr2]
 
 criterion = accuracy_score # Thankfully this task has a pretty easy evaluation... you either get it right or wrong
 
-data_2007_2010 = wayne_all.get_dates([2007, 2008, 2009, 2010])
-expmt_2007_2010 = StratifiedExperiment(lr1, criterion, data_2007_2010[:, :-1], data_2007_2010[:, -1])
+data_time1 = wayne_all.get_dates([2007, 2008, 2009, 2010])
+expmt_time1 = StratifiedExperiment(timemodels[0], criterion, data_time1[:, :-1], data_time1[:, -1], test_size=0.1)
 
-data_2011_2014 = wayne_all.get_dates([2011, 2012, 2013, 2014])
-expmt_2011_2014 = StratifiedExperiment(lr2, criterion, data_2011_2014[:, :-1], data_2011_2014[:, -1])
+data_time2 = wayne_all.get_dates([2011, 2012, 2013, 2014])
+expmt_time2 = StratifiedExperiment(timemodels[1], criterion, data_time2[:, :-1], data_time2[:, -1], test_size=0.1)
 
 
 def do_runs(experiment, runs):
@@ -32,31 +40,44 @@ def do_runs(experiment, runs):
         trainvals.append(trainval)
         testvals.append(testval)
 
-    print("Training Validation: %6.2f, Testing Validation: %6.2f" % (np.mean(trainvals), np.mean(testvals)))
+    print("\nTraining Validation: %6.2f, Testing Validation: %6.2f" % (np.mean(trainvals), np.mean(testvals)))
 
 
+# do_runs(expmt_time1, 10)
+# do_runs(expmt_time2, 10)
+#
+#
+# # Plotting the results of the time experiment
+#
+# plt.figure(0)
+# tickpos = np.arange(len(wayne_all.vector_headers))*2
+# one = plt.barh(tickpos, lr1.coef_[0], align='center', alpha=0.5, height=2)
+# plt.yticks(tickpos, wayne_all.vector_headers)
+#
+# two = plt.barh(tickpos, lr2.coef_[0], align='center', alpha=0.5, height=2)
+#
+# plt.legend((one, two), ('2007-2010', '2011-2014'))
+# plt.xlabel("Model Weight")
+# plt.tick_params(axis='both', which='major', labelsize=8, rotation=45)
+#
+# plt.show()
 
+# Just doing an experiment over all time now that we've established the distributions the observations are drawn from
+# are fairly similar
+model = LogisticRegression()
+experiment = StratifiedExperiment(model, criterion, wayne_all.data[:, :-1], wayne_all.data[:, -1], test_size=0.9)
 
+#Do some runs to get raw accuracies
+do_runs(experiment, 10)
 
-#
-# trainvals = []
-# testvals = []
-# runs = 10
-#
-# for run in range(runs):
-#     trainval, testval = expmt_2007_2010.run()
-#     trainvals.append(trainval)
-#     testvals.append(testval)
-#
-# print("Training Validation: %6.2f, Testing Validation: %6.2f" % (np.mean(trainvals), np.mean(testvals)))
-#
-# trainvals = []
-# testvals = []
-# runs = 10
-#
-# for run in range(runs):
-#     trainval, testval = expmt_2011_2014.run()
-#     trainvals.append(trainval)
-#     testvals.append(testval)
-#
-# print("Training Validation: %6.2f, Testing Validation: %6.2f" % (np.mean(trainvals), np.mean(testvals)))
+# To investigate failures let's look at the confusion matrix from multiple runs as well.
+conmat = np.zeros([2,2])
+for run in range(10):
+    train_idx, test_idx = experiment.partition(wayne_all.data[:, :-1], wayne_all.data[:, -1])
+    model.fit(wayne_all.data[train_idx, :-1], wayne_all.data[train_idx, -1])
+    conmat += confusion_matrix(wayne_all.data[test_idx, -1], model.predict(wayne_all.data[test_idx, :-1]))
+
+conmat = conmat / np.sum(conmat)
+
+print("\n")
+print(conmat)
